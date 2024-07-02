2930b6e98ead103e5ab2c5596f6ee7d0819bb91b
Irfi M <irfi@localcitizen>
2024-07-02 18:54:04 +0700

    feat: east-wasm-sdk package and wasm-test

--- wasm-sdk/assembly/index.ts
+++ wasm-sdk/assembly/index.ts
@@ -1,8 +1,12 @@
 import { consoleLog, valueReturn } from "./env";
 import {
+  Table,
   Column,
+  UTXO,
   toJson,
   ptrToString,
+  toStringSchema,
+  toJsonArray,
   getResultFromJson,
   create,
   selectRow,
@@ -10,67 +14,22 @@ import {
   updateRows,
   deleteRows,
   getTxUTXOByBlockHeight,
-  Table,
 } from "./sdk";
-
-// TODO: create Table class
-const ordinalsTable = new Table("ordinals", [
-  new Column("id", "int64"),
-  new Column("address", "string"),
-  new Column("value", "int64"),
-]);
-
-export function init(): void {
-  ordinalsTable.init("id");
-}
-
-export function getOrdinal(id_ptr: i32): string {
-  const id = ptrToString(id_ptr);
-  return ordinalsTable.select([new Column("id", id)]);
-}
-
-// Testing functions
-export function insertItemTest(): void {
-  ordinalsTable.insert([
-    new Column("id", "0"),
-    new Column("address", "bc1q0d4836j3ekmm9cz7v3kcf0sdsxtmzg4ttpu7dm"),
-    new Column("value", "1000"),
-  ]);
-}
-
-export function updateItemTest(): void {
-  ordinalsTable.update(
-    [new Column("id", "0")],
-    [new Column("address", "bc1qjr4gcelycyck4yxcnx5xt3w26u28veyu7meley")]
-  );
-}
-
-export function deleteItemTest(): void {
-  ordinalsTable.delete([new Column("id", "0")]);
-}
-
-export function selectItemTest(): void {
-  const result = ordinalsTable.select([new Column("id", "0")]);
-  const jsonResult = toJson(result);
-
-  consoleLog(getResultFromJson(jsonResult, "id", "string"));
-  consoleLog(getResultFromJson(jsonResult, "address", "string"));
-  consoleLog(getResultFromJson(jsonResult, "value", "string"));
-}
-
-// Unit test purpose
-export function index(block_height_ptr: i32): void {
-  const block_height: u64 = u64(parseInt(ptrToString(block_height_ptr)));
-  const utxos = getTxUTXOByBlockHeight(block_height);
-
-  for (let i = 0; i < utxos.length; i++) {
-    consoleLog(utxos[i].fundingTxHash);
-  }
-}
-
-export function processString(str_ptr: i32): void {
-  const input = ptrToString(str_ptr);
-  const output = "output for " + input;
-
-  valueReturn(output);
-}
+export {
+  consoleLog,
+  valueReturn,
+  Column,
+  Table,
+  UTXO,
+  toJson,
+  toJsonArray,
+  ptrToString,
+  toStringSchema,
+  getResultFromJson,
+  create,
+  selectRow,
+  insertRow,
+  updateRows,
+  deleteRows,
+  getTxUTXOByBlockHeight,
+};
---
+++ wasm-test/assembly/index.ts
@@ -0,0 +1,79 @@
+// SDK imports
+import {
+  consoleLog,
+  valueReturn,
+  Column,
+  toJson,
+  ptrToString,
+  getResultFromJson,
+  create,
+  selectRow,
+  insertRow,
+  updateRows,
+  deleteRows,
+  getTxUTXOByBlockHeight,
+  Table,
+} from "east-wasm-sdk/assembly";
+export { allocate } from "east-wasm-sdk/assembly/external";
+
+
+const ordinalsTable = new Table("ordinals", [
+  new Column("id", "int64"),
+  new Column("address", "string"),
+  new Column("value", "int64"),
+]);
+
+export function init(): void {
+  ordinalsTable.init("id");
+}
+
+export function getOrdinal(id_ptr: i32): string {
+  const id = ptrToString(id_ptr);
+  return ordinalsTable.select([new Column("id", id)]);
+}
+
+// Testing functions
+export function insertItemTest(): void {
+  ordinalsTable.insert([
+    new Column("id", "0"),
+    new Column("address", "bc1q0d4836j3ekmm9cz7v3kcf0sdsxtmzg4ttpu7dm"),
+    new Column("value", "1000"),
+  ]);
+}
+
+export function updateItemTest(): void {
+  ordinalsTable.update(
+    [new Column("id", "0")],
+    [new Column("address", "bc1qjr4gcelycyck4yxcnx5xt3w26u28veyu7meley")]
+  );
+}
+
+export function deleteItemTest(): void {
+  ordinalsTable.delete([new Column("id", "0")]);
+}
+
+export function selectItemTest(): void {
+  const result = ordinalsTable.select([new Column("id", "0")]);
+  const jsonResult = toJson(result);
+
+  consoleLog(getResultFromJson(jsonResult, "id", "string"));
+  consoleLog(getResultFromJson(jsonResult, "address", "string"));
+  consoleLog(getResultFromJson(jsonResult, "value", "string"));
+}
+
+// Unit test purpose
+export function index(block_height_ptr: i32): void {
+  const block_height: u64 = u64(parseInt(ptrToString(block_height_ptr)));
+  const utxos = getTxUTXOByBlockHeight(block_height);
+
+  for (let i = 0; i < utxos.length; i++) {
+    consoleLog(utxos[i].fundingTxHash);
+  }
+}
+
+export function processString(str_ptr: i32): void {
+  const input = ptrToString(str_ptr);
+  const output = "output for " + input;
+
+  valueReturn(output);
+}
