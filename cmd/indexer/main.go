eca5f251b6b33b09cc93ca3a7b6967684cb833ed
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-07-03 17:26:31 +0700

    update: delete comments

--- cmd/indexer/main.go
+++ cmd/indexer/main.go
@@ -6,9 +6,6 @@ import (
 	storeDB "eastnode/utils/store"
 
 	"github.com/btcsuite/btcd/chaincfg"
-	// "eastnode/indexer/repository"
-	// storeDB "eastnode/utils/store"
-	// "fmt"
 )
 
 func main() {
@@ -22,16 +19,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	// s := storeDB.GetInstance(storeDB.IndexerDB)
-	// indexerRepo := repository.NewIndexerRepository(s.Gorm)
-	//
-	// outpoints, err := indexerRepo.GetOutpointsByTransactionHash("4564e73f976aea029f9614eb81f6cb2b3ac07f008eb0f5ec0149e6b07b037ba6")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//
-	// for _, v := range outpoints {
-	// 	fmt.Println(v.WitnessAsmScripts)
-	// }
 }
