4d3f0556fe3e2cff9e4dc4179a4a48ac2a9f82f7
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-09-05 01:22:05 +0700

    update: tests

--- runtime/rune_runtime_test.go
+++ runtime/rune_runtime_test.go
@@ -5,13 +5,11 @@ import (
 	"eastnode/types"
 	utils "eastnode/utils"
 	store "eastnode/utils/store"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"reflect"
 	"sort"
-	"strconv"
 	"testing"
 )
 
@@ -32,8 +30,6 @@ func clearRuntimeTestRune() {
 }
 
 type outpoint struct {
-	Block  string `json:"block"`
-	Tx     string `json:"tx"`
 	Hash   string `json:"hash"`
 	Vout   string `json:"vout"`
 	Amount string `json:"amount"`
@@ -299,189 +295,4 @@ func TestRuneGetOutpointsByRuneIdAndAddress(t *testing.T) {
 	fmt.Println(res)
 
 	// TODO: assert the data
-}
-
-type runeEntry struct {
-	Number       string `json:"number"`
-	Block        string `json:"block"`
-	Tx           string `json:"tx"`
-	Minted       string `json:"minted"`
-	Burned       string `json:"burned"`
-	Divisibility string `json:"divisibility"`
-	Premine      string `json:"premine"`
-	Rune         string `json:"rune"`
-	Spacers      string `json:"spacers"`
-	Symbol       string `json:"symbol"`
-	Turbo        string `json:"turbo"`
-	Terms        string `json:"terms"`
-	Amount       string `json:"amount"`
-	Cap          string `json:"cap"`
-	HeightStart  string `json:"height_start"`
-	HeightEnd    string `json:"height_end"`
-	OffsetStart  string `json:"offset_start"`
-	OffsetEnd    string `json:"offset_end"`
-}
-
-type diffEntryOutpointData struct {
-	Hash   string `json:"hash"`
-	Index  string `json:"index"`
-	Amount string `json:"amount"`
-}
-
-type diffEntryTermsData struct {
-	Amount      *string `json:"amount"`
-	Cap         *string `json:"cap"`
-	HeightStart *int    `json:"height_start"`
-	HeightEnd   *int    `json:"height_end"`
-	OffsetStart *int    `json:"offset_start"`
-	OffsetEnd   *int    `json:"offset_end"`
-}
-
-type diffEntryData struct {
-	Number       int                     `json:"number"`
-	Block        int                     `json:"block"`
-	Tx           int                     `json:"tx"`
-	Minted       string                  `json:"minted"`
-	Burned       string                  `json:"burned"`
-	Divisibility int                     `json:"divisibility"`
-	Premine      string                  `json:"premine"`
-	Rune         string                  `json:"rune"`
-	Spacers      int                     `json:"spacers"`
-	Symbol       *string                 `json:"symbol"`
-	Turbo        bool                    `json:"turbo"`
-	Terms        *diffEntryTermsData     `json:"terms"`
-	Outpoints    []diffEntryOutpointData `json:"outpoints"`
-}
-
-func TestDumpRuneState840000_840010(t *testing.T) {
-	defer t.Cleanup(clearRuntimeTestRune)
-
-	log.Println("Preparing database & wasm runtime for testing")
-
-	wasmBytes, _ := os.ReadFile("../../runes-smart-index/smartindex/build/release.wasm")
-	wr := getRuneWasmRuntime("../utils/store/test/rune/indexer_840000_840010/doltdump.sql")
-	_, err := wr.RunWasmFunction("", wasmBytes, "temp", "init", []string{}, types.Call)
-	if err != nil {
-		t.Error(err)
-	}
-
-	log.Println("Indexing data")
-	_, err = wr.RunWasmFunction("", wasmBytes, "temp", "index", []string{"840000", "840000"}, types.Call)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// getting outpoints
-	resOutpointsRaw, err := wr.RunSelectFunction("SELECT * from temp_outpoints where spent = 'false'", []string{})
-	if err != nil {
-		t.Error(err)
-	}
-
-	refOutpoints := resOutpointsRaw.([]map[string]interface{})
-	outpointsMap := make(map[string][]outpoint)
-	for _, v := range refOutpoints {
-		outpoint := outpoint{}
-		_ = utils.MapToStruct(v, &outpoint)
-		key := outpoint.Block + "_" + outpoint.Tx
-		outpointsMap[key] = append(outpointsMap[key], outpoint)
-	}
-
-	// gettings rune entries
-	resRuneEntriesRaw, err := wr.RunSelectFunction("SELECT * from temp_rune_entries", []string{})
-	if err != nil {
-		t.Error(err)
-	}
-	refRuneEntries := resRuneEntriesRaw.([]map[string]interface{})
-	entries := make([]diffEntryData, len(refRuneEntries))
-
-	for i, v := range refRuneEntries {
-		entry := runeEntry{}
-		_ = utils.MapToStruct(v, &entry)
-
-		outpoints := outpointsMap[entry.Block+"_"+entry.Tx]
-		entryOutpoints := make([]diffEntryOutpointData, len(outpoints))
-		for i, outpoint := range outpoints {
-			entryOutpoints[i] = diffEntryOutpointData{
-				Hash:   outpoint.Hash,
-				Index:  outpoint.Vout,
-				Amount: outpoint.Amount,
-			}
-		}
-
-		sort.Slice(entryOutpoints, func(i, j int) bool {
-			if entryOutpoints[i].Hash != entryOutpoints[j].Hash {
-				return entryOutpoints[i].Hash < entryOutpoints[j].Hash
-			}
-			iIndex, _ := strconv.Atoi(entryOutpoints[i].Index)
-			jIndex, _ := strconv.Atoi(entryOutpoints[j].Index)
-			return iIndex < jIndex
-		})
-
-		number, _ := strconv.Atoi(entry.Number)
-		block, _ := strconv.Atoi(entry.Block)
-		tx, _ := strconv.Atoi(entry.Tx)
-		divisibility, _ := strconv.Atoi(entry.Divisibility)
-		spacers, _ := strconv.Atoi(entry.Spacers)
-		premine := "0"
-		if entry.Premine != "\\N" {
-			premine = entry.Premine
-		}
-
-		data := diffEntryData{
-			Number:       number,
-			Block:        block,
-			Tx:           tx,
-			Minted:       entry.Minted,
-			Burned:       entry.Burned,
-			Divisibility: divisibility,
-			Premine:      premine,
-			Rune:         entry.Rune,
-			Spacers:      spacers,
-			Symbol:       &entry.Symbol,
-			Turbo:        entry.Turbo == "true",
-			Outpoints:    entryOutpoints,
-		}
-		if entry.Terms == "true" {
-			terms := &diffEntryTermsData{}
-
-			terms.Amount = &entry.Amount
-			terms.Cap = &entry.Cap
-
-			if entry.HeightStart != "\\N" {
-				heightStart, _ := strconv.Atoi(entry.HeightStart)
-				terms.HeightStart = &heightStart
-			}
-
-			if entry.HeightEnd != "\\N" {
-				heightEnd, _ := strconv.Atoi(entry.HeightEnd)
-				terms.HeightEnd = &heightEnd
-			}
-
-			if entry.OffsetStart != "\\N" {
-				offsetStart, _ := strconv.Atoi(entry.OffsetStart)
-				terms.OffsetStart = &offsetStart
-			}
-
-			if entry.OffsetEnd != "\\N" {
-				offsetEnd, _ := strconv.Atoi(entry.OffsetEnd)
-				terms.OffsetEnd = &offsetEnd
-			}
-
-			data.Terms = terms
-		}
-
-		entries[i] = data
-	}
-
-	latestIndexedBlock, err := wr.RunWasmFunction("", wasmBytes, "temp", "get_latest_indexed_block", []string{}, types.Call)
-	if err != nil {
-		t.Error(err)
-	}
-
-	sort.Slice(entries, func(i, j int) bool {
-		return entries[i].Number < entries[j].Number
-	})
-
-	jsonStr, _ := json.MarshalIndent(entries, "", "  ")
-	os.WriteFile(fmt.Sprintf("../../runes-diff-tests/smartindex-states/%s.json", latestIndexedBlock), []byte(jsonStr), 0644)
 }
